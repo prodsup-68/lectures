4a82a414acbfa8c4b1559006f0dbcaf609e016f0
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2025-12-04 07:59:07 +0700

    Update

--- build.ps1
+++ build.ps1
@@ -18,10 +18,10 @@
 # npm run html $folder
 # npm run pdf $folder
 
-$folder = "src\T20_integration"
-npm run html $folder
-npm run pdf $folder
-
-# $folder = "src\T99_project"
+# $folder = "src\T20_integration"
 # npm run html $folder
 # npm run pdf $folder
+
+$folder = "src\T99_project"
+npm run html $folder
+npm run pdf $folder
