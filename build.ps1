63c09678a85b233dc5ce08534fb408dc18b5e95a
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2026-02-09 07:58:01 +0700

    Update

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
-# $folder = "src\T01_intro"
-# npm run html $folder
-# npm run pdf $folder
+$folder = "src\T01_intro"
+npm run html $folder
+npm run pdf $folder
 
 # $folder = "src\T02_erpnext"
 # npm run html $folder
@@ -78,6 +78,6 @@
 # npm run html $folder
 # npm run pdf $folder
 
-$folder = "src\T99_project"
-npm run html $folder
-npm run pdf $folder
+# $folder = "src\T99_project"
+# npm run html $folder
+# npm run pdf $folder
